cef5f292026cd9656cc80929145662f317e12f1f
boppy88 <bopsauce88@gmail.com>
2024-09-22 10:32:02 -0400

    final commit

--- client/src/contact.jsx
+++ client/src/contact.jsx
@@ -14,14 +14,13 @@ export default function Contact() {
 				<label htmlFor="myLName"> Last Name * </label>
 				<input type="text" id="myLName" name="myLName" required="required" /> <br /> <br />
 				
-				
 				<label htmlFor="myEmail"> Email * </label>
 				<input type="email" id="myEmail" name="myEmail" required="required" /> <br /> <br />
 				
                     <label htmlFor="myMessage"> Message (Optional) </label>
 				<input type="text" id="myMessage" name="myMessage" /> <br /> <br />
 
-				<Link to="/"><input type="submit" className="submit" value="Submit" /> <br /></Link>
+				<Link to="/"><input type="submit" className="submit" value="Submit" /></Link> <br />
 		</form>
           <p><i>Text me at: <strong>905-665-1490</strong></i></p>
           <p>Email: jtimiani@my.centennialcollege.ca</p>
